be4c3e90374e308ffec2386f112b435ccf1b1779
Youssef Osama <Youssef.gabr9988@gmail.com>
2024-10-23 22:28:52 +0300

    CourseRepositoryImpl - TeacherRepositoryImpl (add function)

--- Repository/Repository.cpp
+++ Repository/Repository.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class StudentRepository
 {
 public:
-    virtual int addStudent(Student student);
+    virtual int addStudent(Student student) = 0;
 };
 // StudentRepositoryImpl
 class StudentRepositoryImpl : StudentRepository
@@ -16,7 +16,7 @@ private:
     int index = 0;
 
 public:
-    int addStudent(Student student) override
+    int addStudent(Student student)
     {
         if (index == 25)
         {
@@ -29,14 +29,59 @@ public:
         }
     }
 };
+///////////////////////////////// Course /////////////////////////////////
+// interface CourseRepository
+class CourseRepository
+{
+public:
+    virtual int addCourse(Course course) = 0;
+};
+// CourseRepositoryImpl
+class CourseRepositoryImpl
+{
+private:
+    Course courses[25];
+    int index = 0;
+
+public:
+    int addStudent(Course course)
+    {
+        if (index == 25)
+        {
+            cout << "Full Student";
+        }
+        else
+        {
+            courses[index] = course;
+            index++;
+        }
+    }
+};
 ///////////////////////////////// Teacher /////////////////////////////////
 // interface TeacherRepository
 class TeacherRepository
 {
-private:
 public:
+    virtual int addTeacher(Teacher teacher) = 0;
 };
 // TeacherRepositoryImpl
 class TeacherRepositoryImpl
 {
+private:
+    Course teachers[25];
+    int index = 0;
+
+public:
+    int addTeacher(Teacher teacher)
+    {
+        if (index == 25)
+        {
+            cout << "Full Student";
+        }
+        else
+        {
+            teachers[index] = teacher;
+            index++;
+        }
+    }
 };
