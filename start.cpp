5ff392faae5135ebdf4b1eb770b41f055b6988ef
Youssef Osama <Youssef.gabr9988@gmail.com>
2024-10-24 02:36:55 +0300

    displaySystem - showList

--- start.cpp
+++ start.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 using namespace std;
-#define endl "\n"
+void displaySystem()
+{
+    cout << "\t\t**************** Student Management System ****************" << endl;
+    cout << "Please Enter Process You To Do" << endl;
+    cout << "1 - About Students" << "\t\t" << "2 - About Courses" << endl;
+    cout << "3 - About Teachers" << "\t\t" << "4 - Exit" << endl;
+}
+void showList(string value)
+{
+    cout << "1 - Add " << value << "\t\t" << "2 - Remove " << value << endl;
+    cout << "3 - Edit " << value << (value == "Course" ? "\t\t" : "\t") << "4 - Show " << value << endl;
+    cout << "5 - Exit" << endl;
+}
 int main()
 {
     int process;
     bool flag = 1;
     while (flag)
     {
-        cout << "\t\t**************** Student Management System ****************" << endl;
-        cout << "1 - Add Student" << "\t\t" << "2 - Remove Student" << endl;
-        cout << "3 - Edit Student" << "\t" << "4 - Show Student" << endl;
-        cout << "5 - Exit" << endl;
+        displaySystem();
         cin >> process;
         switch (process)
         {
         case 1:
-            cout << "Process Add" << endl;
+            showList("Student");
             break;
         case 2:
-            cout << "Process Remove" << endl;
+            showList("Course");
             break;
         case 3:
-            cout << "Process Edit" << endl;
+            showList("Teacher");
             break;
         case 4:
-            cout << "Process Show" << endl;
-            break;
-        case 5:
             cout << "Process Exit" << endl;
             flag = 0;
             break;
